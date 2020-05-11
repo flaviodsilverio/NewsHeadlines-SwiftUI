cf4056bc4e57719fe7bb9d103bbb80d7baf3de0a
Flavio Silverio <>
2020-05-11 10:43:13 +0200

    Adds navigation to the full article

--- NewsHeadlines/Views and ViewModels/Headlines/Details/HeadlineDetailsView.swift
+++ NewsHeadlines/Views and ViewModels/Headlines/Details/HeadlineDetailsView.swift
@@ -9,55 +9,56 @@
 import Foundation
 import SwiftUI
 import Combine
+import WebKit
 
 class ImageLoader: ObservableObject {
-    @Published var image: UIImage?
+	@Published var image: UIImage?
 
-    private var cancellable: AnyCancellable?
-    private let url: URL
+	private var cancellable: AnyCancellable?
+	private let url: URL
 
-    init(url: URL) {
-        self.url = url
-    }
+	init(url: URL) {
+		self.url = url
+	}
 
-    deinit {
-        cancellable?.cancel()
-    }
+	deinit {
+		cancellable?.cancel()
+	}
 
-    func load() {
-        cancellable = URLSession.shared.dataTaskPublisher(for: url)
-            .map { UIImage(data: $0.data) }
-            .replaceError(with: nil)
-            .receive(on: DispatchQueue.main)
-            .assign(to: \.image, on: self)
-    }
+	func load() {
+		cancellable = URLSession.shared.dataTaskPublisher(for: url)
+			.map { UIImage(data: $0.data) }
+			.replaceError(with: nil)
+			.receive(on: DispatchQueue.main)
+			.assign(to: \.image, on: self)
+	}
 
-    func cancel() {
-        cancellable?.cancel()
-    }
+	func cancel() {
+		cancellable?.cancel()
+	}
 }
 
 struct AsyncImage<Placeholder: View>: View {
-    @ObservedObject private var loader: ImageLoader
-    private let placeholder: Placeholder?
+	@ObservedObject private var loader: ImageLoader
+	private let placeholder: Placeholder?
 	private var contentMode: ContentMode?
 
 	init(url: URL,
 		 placeholder: Placeholder? = nil,
 		 contentMode: ContentMode? = .fill) {
 
-        loader = ImageLoader(url: url)
-        self.placeholder = placeholder
+		loader = ImageLoader(url: url)
+		self.placeholder = placeholder
 		self.contentMode = contentMode
-    }
+	}
 
-    var body: some View {
-        image
-            .onAppear(perform: loader.load)
-            .onDisappear(perform: loader.cancel)
-    }
+	var body: some View {
+		image
+			.onAppear(perform: loader.load)
+			.onDisappear(perform: loader.cancel)
+	}
 
-    private var image: some View {
+	private var image: some View {
 		guard let image = loader.image else { return AnyView(placeholder) }
 
 		return AnyView(
@@ -65,11 +66,12 @@ struct AsyncImage<Placeholder: View>: View {
 				.resizable()
 				.aspectRatio(contentMode: contentMode ?? .fill))
 
-    }
+	}
 }
 
 struct HeadlineDetailsView: View {
 	var content: Headline?
+	@State var isPresenting: Bool = false
 
 	var body: some View {
 		VStack(spacing: 16) {
@@ -80,14 +82,24 @@ struct HeadlineDetailsView: View {
 			Text(content?.publishedAt ?? "")
 			Text(content?.description ?? "")
 			Spacer()
-			Button(action: {
+			//			NavigationLink(destination: WebView(url: content?.url)) {
+			//				Text("Read Full Article")
+			//					.foregroundColor(.blue)
+			//			}.buttonStyle(PlainButtonStyle())
+			//			NavigationLink(destination: Modal(isPresented: false)) {
+			//				Text("Read Full Article")
+			//			}
 
-			}) {
-				Text("Read Full Article")
-					.frame(alignment: .trailing)
+			Button("label", action: {
+				self.isPresenting.toggle()
+			})
+				.sheet(isPresented: $isPresenting, onDismiss: {
 
-			}
-			.frame(alignment: .trailing)
+				}, content: {
+					WebView(url: self.content?.url)
+				})
+
+				.frame(alignment: .trailing)
 
 			Spacer().frame(height: 20)
 		}
@@ -97,5 +109,19 @@ struct HeadlineDetailsView: View {
 struct HeadlineDetailsView_Previews: PreviewProvider {
 	static var previews: some View {
 		HeadlineDetailsView(content: nil)
+	}
+}
+
+struct WebView: UIViewRepresentable {
+	var url: String?
+
+	func updateUIView(_ uiView: UIViewType, context: Context) {
+		guard let url = URL(string: url ?? "") else { return }
+
+		uiView.load(URLRequest(url: url))
+	}
+
+	func makeUIView(context: Context) -> some WKWebView {
+		return WKWebView()
 	}
 }
